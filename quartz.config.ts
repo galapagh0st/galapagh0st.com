d20cd1d9c3941f259ec0483786f13f1ea4a5dd05
NARKWHAL <n4rkwh41@proton.me>
2025-02-24 03:10:31 -0500

    add my own menu bar stuff

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "👻 GalapaGh0st's Notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
