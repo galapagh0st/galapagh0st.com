134b7c644ae0ae8b005083e0795d095621048d78
alexMcosta <alexmcosta.dev@gmail.com>
2025-02-25 16:12:18 -0500

    noticed a weird file

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,7 +14,7 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          "The digital notebook of Alex Costa \n"
+          <p>The digital notebook of Alex Costa</p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
           <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
         </p>
